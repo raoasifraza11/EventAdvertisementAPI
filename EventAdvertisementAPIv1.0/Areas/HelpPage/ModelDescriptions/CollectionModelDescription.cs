78c35bf25a6fc8584a9724f62b8437d457b2e0e2
raoasifraza11 <raoasifraza1@hotmail.com>
2017-03-12 11:59:10 +0500

    Add project files.

---
+++ EventAdvertisementAPIv1.0/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace EventAdvertisementAPIv1._0.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ EventAdvertisementAPIv1.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EventAdvertisementAPIv1._0.WebApiApplication" Language="C#" %>
